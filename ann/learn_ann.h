ccc11d5a4ac4bd1b06a872f4bd32f988391dec4e
Matthew Lai <m@matthewlai.ca>
2016-09-03 19:30:56 +0100

    Removed more dead code

--- ann/learn_ann.h
+++ ann/learn_ann.h
@@ -29,13 +29,6 @@ EvalNet BuildEvalNet(int64_t inputDims, int64_t outputDims, bool smallNet);
 
 MoveEvalNet BuildMoveEvalNet(int64_t inputDims, int64_t outputDims);
 
-template <typename Derived1, typename Derived2>
-void TrainANN(
-	const Eigen::MatrixBase<Derived1> &x,
-	const Eigen::MatrixBase<Derived2> &y,
-	EvalNet &nn,
-	int64_t epochs);
-
 }
 
 #endif // LEARN_ANN_H
